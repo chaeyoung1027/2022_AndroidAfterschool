3abaafdf4d3ac9a7faeecd19cf84fa4e4f07b626
ANR <rnldyal1027@naver.com>
2022-08-01 10:44:40 +0900

    깃허브 정보를 받아 login, id, name 나오게 하기 !이름이 NULL일 때 오류 고치기

--- GitHubUserSearch/app/src/main/java/com/example/githubusersearch/GitHubAPIService.kt
+++ GitHubUserSearch/app/src/main/java/com/example/githubusersearch/GitHubAPIService.kt
@@ -5,32 +5,36 @@ import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Path
 import java.lang.reflect.Type
 
 interface GitHubAPIService {
     @GET("/users/{user}")
-    fun getGitHubServiceInfo(
-        @Path("user") user: String
-    ) : Call<GitHubServiceData>
+    fun getUser(
+        @Path("user") id: String,
+        @Header("Authorization") pat : String
+    ) : Call<GitHubUser>
 }
 
-data class GitHubServiceData(
+data class GitHubUser(
     val id: Int,
-    val login: String
+    val login: String,
+    val name : String?
 )
 
-class GitHubServiceDeserializerGSON : JsonDeserializer<GitHubServiceData> {
+class GitHubUserDeserializer : JsonDeserializer<GitHubUser> {
     override fun deserialize(
         json: JsonElement?,
         typeOfT: Type?,
         context: JsonDeserializationContext?
-    ): GitHubServiceData {
+    ): GitHubUser {
         val node = json?.getAsJsonObject()
         val id = node?.getAsJsonPrimitive("id")?.asInt
         val login = node?.getAsJsonPrimitive("login")?.asString
+        val name = node?.getAsJsonPrimitive("name")?.asString
 
-        return GitHubServiceData(id!!, login!!)
+        return GitHubUser(id!!, login!!, name)
     }
 
 }
--- GitHubUserSearch/app/src/main/java/com/example/githubusersearch/MainActivity.kt
+++ GitHubUserSearch/app/src/main/java/com/example/githubusersearch/MainActivity.kt
@@ -2,19 +2,54 @@ package com.example.githubusersearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import com.google.gson.GsonBuilder
+import org.w3c.dom.Text
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchEdit = findViewById<EditText>(R.id.search_text)
-        val searchButton = findViewById<Button>(R.id.search_button)
+        val userIdInput = findViewById<EditText>(R.id.user_id_input)
+        val content = findViewById<TextView>(R.id.content)
 
-        searchButton.setOnClickListener {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .addConverterFactory(
+                GsonConverterFactory.create(
+                    GsonBuilder().registerTypeAdapter(
+                        GitHubUser::class.java,
+                        GitHubUserDeserializer()
+                    ).create()
+                )
+            ).build()
 
+        val apiService = retrofit.create(GitHubAPIService::class.java)
+
+        findViewById<Button>(R.id.search_btn).setOnClickListener {
+            val id = userIdInput.text.toString()
+            val apiCallForData = apiService.getUser(id,"token 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")
+            apiCallForData.enqueue(object : Callback<GitHubUser>{
+                override fun onResponse(call: Call<GitHubUser>, response: Response<GitHubUser>) {
+                    val data = response.body()!!
+                    Log.d("mytag", data.toString())
+                    content.text = "login: ${data.login}\nid:${data.id}\nname:${data.name}\n"
+                }
+
+                override fun onFailure(call: Call<GitHubUser>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+            })
         }
     }
 }
