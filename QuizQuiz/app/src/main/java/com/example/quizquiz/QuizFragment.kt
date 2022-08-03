c3bfab1de438719cbb4a81d4cd0bc4d47e81c86d
ANR <rnldyal1027@naver.com>
2022-08-03 11:14:13 +0900

    퀴즈를 풀면 Log로 정답, 오답을 알 수 있다. 해야할 것 : 문제 풀면 다음 문제로 넘어가게 하기

--- QuizQuiz/app/src/main/java/com/example/quizquiz/QuizFragment.kt
+++ QuizQuiz/app/src/main/java/com/example/quizquiz/QuizFragment.kt
@@ -1,13 +1,58 @@
 package com.example.quizquiz
 
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.loader.content.AsyncTaskLoader
+import com.example.quizquiz.database.Quiz
+import com.example.quizquiz.database.QuizDatabase
 
-class QuizFragment : Fragment() {
+class QuizFragment : Fragment(),
+    QuizStartFragment.QuizStartListener,
+    QuizSolveFragment.QuizSolveListener{
+    lateinit var db : QuizDatabase
+    lateinit var quizList:List<Quiz>
+    var currentQuizIdx = 0
+    var correctCount = 0
+
+    //QuizSolveListener 구현하고, 맞췄으면 "정답!", 틀렸으면 "오답!" <=로그로 출력
+    override fun onAnswerSelected(isCorrect: Boolean) {
+        if(isCorrect) Log.d("mytag", "정답!")
+        else Log.d("mytag", "오답!")
+    }
+
+    override fun onQuizStart() {
+        Log.d("mytag", "시작하기!!!")
+
+        AsyncTask.execute {
+            currentQuizIdx = 0
+            correctCount = 0
+            quizList = db.quizDAO().getAll()
+
+            childFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container,
+                    QuizSolveFragment.newInstance(quizList[currentQuizIdx]))
+                .commit()
+        }
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
-        return inflater.inflate(R.layout.quiz_fragment, container, false)
+        val view = inflater.inflate(
+            R.layout.quiz_fragment,
+            container,
+            false)
+
+        db = QuizDatabase.getInstance(requireContext())
+
+        childFragmentManager
+            .beginTransaction().add(R.id.fragment_container, QuizStartFragment())
+            .commit()
+
+        return view
     }
 }
--- QuizQuiz/app/src/main/java/com/example/quizquiz/QuizSolveFragment.kt
+++ QuizQuiz/app/src/main/java/com/example/quizquiz/QuizSolveFragment.kt
@@ -1,7 +1,14 @@
 package com.example.quizquiz
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
 import com.example.quizquiz.database.Quiz
 
 class QuizSolveFragment : Fragment() {
@@ -20,5 +27,56 @@ class QuizSolveFragment : Fragment() {
         } else {
             throw Exception("QuizStartListener 미구현")
         }
+    }
+    //newInstance 클래스 매서드(퀴즈 객체를 전달받도록 구현)
+    companion object{
+        fun newInstance(quiz:Quiz):QuizSolveFragment{
+            val fragment = QuizSolveFragment()
+
+            val args = Bundle()
+            args.putParcelable("quiz", quiz)
+            fragment.arguments = args
+
+            return fragment
+        }
+    }
+    override fun onCreateView(
+        inflater:LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState:Bundle?
+    ): View?{
+        val view = inflater.inflate(
+            R.layout.quiz_solve_fragment, container, false)
+        quiz = arguments?.getParcelable("quiz")!!
+        view.findViewById<TextView>(R.id.question).text = quiz.question
+        val choices = view.findViewById<ViewGroup>(R.id.choices)
+        //choices 가 LinearLayout, viewGroup(상위타입)을 상속받아서 ViewGroup 와 LinearLayout 둘 다 써도 된다.
+
+        val answerSelectListener = View.OnClickListener {
+            val guess = (it as Button).text.toString()
+            //guess와 퀴즈의 실제 답을 비교해서 listener의 onAnswerSelected를 적절히 호출
+            if(guess==quiz.answer) listener.onAnswerSelected(true)
+            else listener.onAnswerSelected(false)
+        }
+
+        when(quiz.type){
+            "ox"->{
+                for(sign in listOf("o","x")){
+                    var btn = Button(activity)
+                    btn.text = sign
+                    btn.setOnClickListener (answerSelectListener)
+                    choices.addView(btn)
+                }
+            }
+            "multiple_choice"-> {
+                    for (sign in quiz.guesses!!) {
+                        var btn = Button(activity)
+                        btn.text = sign
+                        btn.setOnClickListener (answerSelectListener)
+                        choices.addView(btn)
+                    }
+                }
+            }
+        return view
     }
 }
