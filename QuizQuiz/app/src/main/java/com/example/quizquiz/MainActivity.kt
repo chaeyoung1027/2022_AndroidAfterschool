df6fd1bd3f6918c577922c798aa503ae0a8801c4
ANR <rnldyal1027@naver.com>
2022-08-03 14:42:12 +0900

    퀴즈 풀기 앱 완성! 퀴즈 추가하기 기능 추가!

--- QuizQuiz/app/src/main/java/com/example/quizquiz/MainActivity.kt
+++ QuizQuiz/app/src/main/java/com/example/quizquiz/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
@@ -21,71 +22,79 @@ class MainActivity : AppCompatActivity() {
     lateinit var drawerToggle : ActionBarDrawerToggle
     lateinit var db : QuizDatabase
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-        db = QuizDatabase.getInstance(this)
+            db = QuizDatabase.getInstance(this)
+
+            Thread(Runnable {
+                for(quiz in db.quizDAO().getAll()) {
+                    Log.d("mytag", quiz.toString())
+                }
+            }).start()
+
 //        db.quizDAO().insert(
 //            Quiz(type="OX", question="asdf", answer = "", category="?+")
 //        )
 
-        Thread(Runnable {
-            for(quiz in db.quizDAO().getAll()){
-                Log.d("mytag", quiz.toString())
-            }
-        }).start()
-
-        val sp : SharedPreferences = getSharedPreferences(
-            "pref", Context.MODE_PRIVATE)
-        if(sp.getBoolean("initialized", true)) {
-            initQuizDataFromXMLFile()
-            val editor = sp.edit()
-            editor.putBoolean("initialized", false)
-            editor.commit()
-        }
-
-        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val navView = findViewById<NavigationView>(R.id.drawer_nav_view)
-
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.frame, QuizFragment())
-            .commit()
-
-        navView.setNavigationItemSelectedListener {
-            when(it.itemId) {
-                R.id.quiz_solve -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame, QuizFragment())
-                        .commit()
-                }
-                R.id.quiz_manage -> {
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.frame, QuizListFragment())
-                        .commit()
-                }
+            val sp : SharedPreferences = getSharedPreferences(
+                "pref", Context.MODE_PRIVATE)
+            if(sp.getBoolean("initialized", true)) {
+                initQuizDataFromXMLFile()
+                val editor = sp.edit()
+                editor.putBoolean("initialized", false)
+                editor.commit()
             }
 
-            drawerLayout.closeDrawers()
+            val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+            val navView = findViewById<NavigationView>(R.id.drawer_nav_view)
 
-            true
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.frame, QuizFragment())
+                .commit()
+
+            navView.setNavigationItemSelectedListener {
+                when(it.itemId) {
+                    R.id.quiz_solve -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.frame, QuizFragment())
+                            .commit()
+                    }
+                    R.id.quiz_manage -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.frame, QuizListFragment())
+                            .commit()
+                    }
+                    R.id.quiz_add -> {
+                        supportFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.frame, QuizCreateFragment())
+                            .commit()
+                    }
+
+                }
+
+                drawerLayout.closeDrawers()
+
+                true
+            }
+
+            drawerToggle = object : ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.drawer_open,
+                R.string.drawer_close
+            ) {}
+            // isDrawerIndicatorEnabled 속성을 true로 설정해 액션바의 왼쪽 상단에 위치한 햄버거 아이콘을 통해 내비게이션 드로어를 표시하고 숨길 수 있도록 합니다.
+            drawerToggle.isDrawerIndicatorEnabled = true
+            drawerLayout.addDrawerListener(drawerToggle)
+            // setDisplayHomeAsUpEnabled 메서드를 호출해서 햄버거 아이콘을 표시하고 해당 아이콘을 클릭해 내비게이션 드로어를 열고 닫을 수 있도록 설
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
         }
-
-        drawerToggle = object : ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            R.string.drawer_open,
-            R.string.drawer_close
-        ) {}
-        // isDrawerIndicatorEnabled 속성을 true로 설정해 액션바의 왼쪽 상단에 위치한 햄버거 아이콘을 통해 내비게이션 드로어를 표시하고 숨길 수 있도록 합니다.
-        drawerToggle.isDrawerIndicatorEnabled = true
-        drawerLayout.addDrawerListener(drawerToggle)
-        // setDisplayHomeAsUpEnabled 메서드를 호출해서 햄버거 아이콘을 표시하고 해당 아이콘을 클릭해 내비게이션 드로어를 열고 닫을 수 있도록 설
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-    }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
--- QuizQuiz/app/src/main/java/com/example/quizquiz/QuizCreateFragment.kt
+++ QuizQuiz/app/src/main/java/com/example/quizquiz/QuizCreateFragment.kt
@@ -1,4 +1,38 @@
 package com.example.quizquiz
 
-class QuizCreateFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+import com.example.quizquiz.database.Quiz
+import com.example.quizquiz.database.QuizDatabase
+
+class QuizCreateFragment :Fragment(){
+    lateinit var db: QuizDatabase
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(
+            R.layout.quiz_add_fragment, container, false)
+        db = QuizDatabase.getInstance(requireContext())
+
+        view.findViewById<Button>(R.id.add_quiz_btn).setOnClickListener {
+            val question = view.findViewById<EditText>(R.id.question).text.toString()
+            val answer = view.findViewById<EditText>(R.id.answer).text.toString()
+            val category = view.findViewById<EditText>(R.id.category).text.toString()
+
+            val quiz = Quiz(type="ox", question=question, answer=answer, category=category)
+            Thread {
+                db.quizDAO().insert(quiz)
+            }.start()
+        }
+
+        return view
+    }
 }
